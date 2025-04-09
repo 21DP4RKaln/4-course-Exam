3813270f31f4836adc9ffce7e71e1b6817093442
Rolands Kalniņš <103189045+21DP4RKaln@users.noreply.github.com>
2025-04-09 14:49:23 +0300

    beidzot

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
 import { ReactNode } from 'react';
-import { redirect } from 'next/navigation';
-import { defaultLocale } from './i18n/config';
 
 type Props = {
   children: ReactNode;
 };
 
 export default function RootLayout({ children }: Props) {
-  redirect(`/${defaultLocale}`);
-
   return <>{children}</>;
 }
