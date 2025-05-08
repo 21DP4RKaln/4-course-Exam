4625b2458ce5c402b17faf35af1ee86fc20bd693
Rolands Kalniņš <103189045+21DP4RKaln@users.noreply.github.com>
2025-05-08 17:56:56 +0300

    docker

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-    images: {
-      domains: ['localhost'],
-      remotePatterns: [
-        {
-          protocol: 'http',
-          hostname: 'localhost',
-          port: '3001',
-          pathname: '/uploads/**',
-        }
-      ]
-    }
-  }
-  
-  export default nextConfig;
+  reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ['localhost', 'app'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'app',
+        port: '3000',
+        pathname: '/uploads/**',
+      }
+    ]
+  },
+  // Enable output to standalone mode for Docker
+  output: 'standalone',
+}
+
+export default nextConfig;
